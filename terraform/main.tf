e801b41224d40eba9912ac918227ad54adc27513
Mikhail <michail.77@mail.ru>
2024-03-14 23:01:35 +0300

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-provider "yandex" {
-  token     = "AQAAAAAIkgGxAATuwW6RRMSneE2uuV6vpMTK0Jw"
-  cloud_id  = "b1ghns2saijtpp8com7i"
-  folder_id = "b1gb5mplcv6bu0g0eolj"
-  zone      = "ru-central1-a" 
-}
-
 resource "yandex_vpc" "my_vpc" {
   name        = "my-vpc"
   folder_id   = var.yandex_folder_id
