ed3bef78b39e602e1ea4ba5048e3b184c287e053
michail <michail.77@mail.ru>
2024-03-18 17:40:18 +0300

    1

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "yandex" {
 #  service_account_key_file = "key.json"
-  token     = "${var.yandex_token}"
-  cloud_id  = "${var.yandex_cloud_id}"
-  folder_id = "${var.yandex_folder_id}"
+  token     = "${var.token}"
+  cloud_id  = "${var.cloud_id}"
+  folder_id = "${var.folder_id}"
   zone      = var.a-zone
 }
